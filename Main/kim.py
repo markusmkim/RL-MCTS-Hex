9b90e55ea7d09c360f2ccbc39eefa40d8da8014b
markusmkim <markus.malum.kim@gmail.com>
2021-04-09 18:14:56 +0200

    example in kim.py

--- Main/kim.py
+++ Main/kim.py
@@ -3,29 +3,12 @@ from Agent.actor import Actor
 from Agent.critic import Critic
 from Main.config import config
 from Tournaments.tournaments import Tournaments
-from SimWorld.utils import generate_board, visualize_board
-import numpy as np
+
 
 tournaments = Tournaments(config)
-# tournaments.run_elite_tournament()
 
 emma = Actor(0, 0, name="emma")
-critic = Critic(name="demo")
+critic = Critic(name="triana")
+
 tournaments.run_interaction_game(emma, actor_starts=True, critic=critic)
 
-
-"""
-grid = [1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0,
-        0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0,
-        0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0,
-        0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0,
-        0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
-
-inp = [0, 1] + grid
-
-#visualize_board(generate_board(grid))
-
-#print(critic.test_evaluate(np.array(inp)))
-
-"""
