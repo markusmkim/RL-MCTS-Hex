c2adec6b856b6bbd70d4c285378ebd3fe2428d8c
markusmkim <markus.malum.kim@gmail.com>
2021-04-19 22:56:54 +0200

    small fix

--- Main/MCTS_inGame.py
+++ Main/MCTS_inGame.py
@@ -6,7 +6,7 @@ from Main.utils import plot_histories, initialize_actor, initialize_critic, save
 from RL.algorithm import run_rl_algorithm
 from Agent.actor import Actor
 
-a = Actor(0, 0, name="emma", randomized=True)
+a = Actor(0, 0, randomized=True)
 
 nora_v2 = Actor(0, 0, name="emma")
 
