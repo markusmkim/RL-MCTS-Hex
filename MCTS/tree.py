eda07560a1f986727df2dbd1aeff6043231e5d77
markusmkim <markus.malum.kim@gmail.com>
2021-08-09 13:16:15 +0200

    add comments

--- MCTS/tree.py
+++ MCTS/tree.py
@@ -10,18 +10,25 @@ class Tree:
 
 
     def mcts(self, start_time, get_next_state, c, rollout_prob):
+        """
+        Runs Monte Carlo Tree Search from the current root node.
+        When the search has terminated, the best child of the root is selected as the new root, and the branches of the
+        tree that are now unreachable are pruned.
+        Returns the number of visits to each child node, the total number of visits, and the action that leads to the
+        best (the most visited) child.
+        """
         if self.root.children is None:
             self.root.expand(get_next_state)
         node = self.root
-        while time() - start_time < 1:
-            while node.children and len(node.children) > 0:
+        while time() - start_time < 1:                          # run as many simulations as possible within 1 second
+            while node.children and len(node.children) > 0:                 # traverse tree to find a leaf node
                 node = node.best_child(c)
-            if node.number_of_visits == 1:
-                node.expand(get_next_state)
+            if node.number_of_visits == 1:                                  # if leaf node is visited before
+                node.expand(get_next_state)                                     # expand node
                 if len(node.children) > 0:
                     node = node.children[0]
-            value = node.rollout(self.actor, self.critic, rollout_prob)
-            while node.parent is not None:
+            value = node.rollout(self.actor, self.critic, rollout_prob)     # do rollout from leaf node
+            while node.parent is not None:                                  # back-propagate values
                 node = node.parent
                 node.value += value
                 node.number_of_visits += 1
